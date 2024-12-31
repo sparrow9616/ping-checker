cc10e212e9af3b68faaabd2f5540dc6bad0c609f
sparrow9616 <gamingleague09@gmail.com>
2024-12-31 21:38:48 +0530

    Add logging to main.py

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from os import getenv
 from dotenv import load_dotenv
 import asyncio
+import logging
 
 from pyrogram import Client
 from pyrogram.errors import FloodWait
 import datetime
 import pytz
 
+# Load environment variables
 load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH") 
 STRING = getenv("STRING_SESSION", None)
-PING_CHANNEL= int(getenv("PING_CHANNEL", None))
+PING_CHANNEL = int(getenv("PING_CHANNEL", None))
 BOT_LIST = [x.strip() for x in getenv("BOT_LIST").split(" ")]
-MSG_ID=int(getenv("MESSAGE_ID",None))
+MSG_ID = int(getenv("MESSAGE_ID", None))
 TIME = 600
 
 loop = asyncio.get_event_loop()
 app = Client(
-            "Stranger" , 
-            api_id=API_ID , 
+            "Stranger", 
+            api_id=API_ID, 
             api_hash=API_HASH, 
             session_string=STRING
             )
 
-
 async def ping_check():
+    logger.info("Starting the ping check script.")
     await app.start()
     while True:
         TEXT = "✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴀᴄᴇ ɴᴇᴛᴡᴏʀᴋ ʙᴏᴛ's sᴛᴀᴛᴜs ᴄʜᴀɴɴᴇʟ"
@@ -34,9 +40,9 @@ async def ping_check():
         for bot in BOT_LIST:
             try:
                 TEXT += f"\n\n ╭⎋ Bot Name: @{bot}"
-                await app.send_message(f"@{bot}","/start")
+                await app.send_message(f"@{bot}", "/start")
                 await asyncio.sleep(4)
-                messages = app.get_chat_history(f"@{bot}",limit=1)
+                messages = app.get_chat_history(f"@{bot}", limit=1)
                 async for message in messages:
                     msg = message.text
                 if msg == "/ping":
@@ -46,13 +52,13 @@ async def ping_check():
             except FloodWait as e:
                 await asyncio.sleep(e.x)
             except Exception as e:
-                print(f"Failed to send ping to {bot}: {e}")
+                logger.error(f"Failed to send ping to {bot}: {e}")
         sp = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
         date = sp.strftime("%d %b %Y")
         time = sp.strftime("%I : %M : %S %p")
         TEXT += f"\n\n--Last checked on--: \n{date}\n{time}"
         await app.edit_message_text(PING_CHANNEL, MSG_ID, TEXT)
-        await asyncio.sleep(TIME)  # Wait for 5 minutes
+        await asyncio.sleep(TIME)  # Wait for 10 minutes
 
-if __name__== "__main__":
+if __name__ == "__main__":
     loop.run_until_complete(ping_check())
